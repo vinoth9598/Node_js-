71581551169eba46d4e7b3793af3e707e0d12e24
vinoth9598 <pvinoth9598@gmail.com>
2026-05-18 11:26:43 +0530

    json response

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 
 const http = require("http");
 
+const notes = [
+    {
+        id:1, 
+        content:"Backend using node.js",
+        important:true 
+    },
+    {
+        id:2,
+        content:"node.js is a open source",
+        important:false 
+    },
+    {
+        id:3,
+        content:"simple web server using node.js",
+        important:true
+    }
+];
+
 const hostName = "127.0.0.1";
 const port = 3001 ;
 
 const server = http.createServer((req,res)=>{
     res.statusCode = 200 ;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
+    res.end(JSON.stringify(notes));
 
 }) ;
 
